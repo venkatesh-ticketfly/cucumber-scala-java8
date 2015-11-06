0a37178d62e446da42fc62011f0dfa795dd6c69d
Venkatesh Amudhan <venkatesh@ticketfly.com>
2015-11-05 18:10:58 -0800

    Specify separate output target for each feature file

--- src/test/scala/info/cucumber/scala/api/CukesFeature.scala
+++ src/test/scala/info/cucumber/scala/api/CukesFeature.scala
@@ -8,5 +8,7 @@ import org.junit.runner.RunWith
 @CucumberOptions(strict = true,
   features = Array("src/test/resources/info/cucumber/scala/cukes.feature"),
   tags = Array("~@ignore"),
-  plugin = Array("pretty", "html:target/cucumber_html", "junit:target/cucumber_junit"))
+  plugin = Array("pretty",
+    "html:target/html/CukesFeature/",
+    "junit:target/junit/CukesFeature.xml"))
 class CukesFeature
--- src/test/scala/info/cucumber/scala/guice/GuiceFeatures.scala
+++ src/test/scala/info/cucumber/scala/guice/GuiceFeatures.scala
@@ -8,5 +8,7 @@ import org.junit.runner.RunWith
 @CucumberOptions(strict = true,
   features = Array("src/test/resources/info/cucumber/scala/guice"),
   tags = Array("~@ignore"),
-  plugin = Array("pretty", "html:target/cucumber_html", "junit:target/cucumber_junit/"))
+  plugin = Array("pretty",
+    "html:target/html/GuiceFeatures/",
+    "junit:target/junit/GuiceFeatures.xml"))
 class GuiceFeatures
